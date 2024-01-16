123f11c59d4e6a24c95e1704b185489816460c76
Ajal Jose <ajaljosem@gmail.com>
2024-01-16 22:00:56 +0530

    setup

---
+++ app/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.scss';
+function Home() {
+  return (
+    <div className='navbar'>
+        <h3 className='navbar__logo'>LOGO</h3>
+        <h3 className='navbar__name'>NAME</h3>
+        <h3 className='navbar__menu'>MENU</h3>
+    </div>)
+}
+
+export default Home;
