8b6c86d28e11c4cff1601d566743e540b7c62190
Ajal Jose <ajaljosem@gmail.com>
2024-01-17 08:08:17 +0530

    minor css

--- app/Home/Home.js
+++ app/Home/Home.js
@@ -2,11 +2,15 @@ import React from 'react'
 import './home.scss';
 function Home() {
   return (
-    <div className='navbar'>
-        <h3 className='navbar__logo'>LOGO</h3>
-        <h3 className='navbar__name'>NAME</h3>
-        <h3 className='navbar__menu'>MENU</h3>
-    </div>)
+    <div>
+      <div className="navbar">
+        <h3 className="navbar__logo">LOGO</h3>
+        <h3 className="navbar__name">NAME</h3>
+        <h3 className="navbar__menu">MENU</h3>
+      </div>
+      <div className='test'></div>
+    </div>
+  );
 }
 
 export default Home;
