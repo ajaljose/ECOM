d0fe142d063dd0bd106ab19340db9599364bdfac
Ajal Jose <ajaljosem@gmail.com>
2024-01-31 21:28:38 +0530

    search bar on nav

--- app/Home/Home.js
+++ app/Home/Home.js
@@ -61,150 +61,220 @@ function Home() {
     
   };
   return (
-    <div className='home'>
+    <div className="home">
       <div className="navbar">
-        <img src={github.src} alt="GitHub Logo" className='navbar__logo' title='URBAN KING'/>
-        <div className='navbar__menu'>
+        <img
+          src={github.src}
+          alt="GitHub Logo"
+          className="navbar__logo"
+          title="URBAN KING"
+        />
+
+        <div className="navbar__menu">
+          <div className="container">
+            <input
+              type="text"
+              name="text"
+              className="input"
+              required
+              placeholder="Type to search..."
+            />
+            <div className="icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="ionicon"
+                viewBox="0 0 512 512"
+              >
+                <title>Search</title>
+                <path
+                  d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeMiterlimit="10"
+                  strokeWidth="32"
+                ></path>
+                <path
+                  fill="#000000"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  strokeWidth="32"
+                  d="M338.29 338.29L448 448"
+                ></path>
+              </svg>
+            </div>
+          </div>
           <h3>Home</h3>
           <h3>Shop</h3>
           <h3>Blog</h3>
           <h3>Contact</h3>
         </div>
       </div>
-      <div className='hero' style={gradientStyle}>
-        <div className='hero__images'>
-          <img src={left.src} alt="HeadPhones" className='hero__images_1' onClick={handleClick}/>
-          <img src={currentImage.src} alt="HeadPhones" className='hero__images_2' />
-          <img src={right.src} alt="HeadPhones" className='hero__images_3'  onClick={handleClick}/>
+      <div className="hero" style={gradientStyle}>
+        <div className="hero__images">
+          <img
+            src={left.src}
+            alt="HeadPhones"
+            className="hero__images_1"
+            onClick={handleClick}
+          />
+          <img
+            src={currentImage.src}
+            alt="HeadPhones"
+            className="hero__images_2"
+          />
+          <img
+            src={right.src}
+            alt="HeadPhones"
+            className="hero__images_3"
+            onClick={handleClick}
+          />
         </div>
-        <h1 className='hero__product'>{prodName}</h1>
-        <button className='hero__btn1'>Shop Now</button>
-        <button className='hero__btn2'>Categories</button>
+        <h1 className="hero__product">{prodName}</h1>
+        <button className="hero__btn1">Shop Now</button>
+        <button className="hero__btn2">Categories</button>
       </div>
-      <div className='features'>
-        <div className='features__shipping'>
-            <img src={shipping.src} className='features__img'></img>
-            <div className='features__text'>
-             <h1>Free shipping</h1>
-             <h3>On Purschase Over 500</h3>
-            </div>
+      <div className="features">
+        <div className="features__shipping">
+          <img src={shipping.src} className="features__img"></img>
+          <div className="features__text">
+            <h1>Free shipping</h1>
+            <h3>On Purschase Over 500</h3>
+          </div>
         </div>
-        <div className='features__shipping'>
-            <img src={support.src} className='features__img'></img>
-            <div className='features__text'>
-             <h1>Online Support</h1>
-             <h3>24/7 Technical Support</h3>
-            </div>
+        <div className="features__shipping">
+          <img src={support.src} className="features__img"></img>
+          <div className="features__text">
+            <h1>Online Support</h1>
+            <h3>24/7 Technical Support</h3>
+          </div>
         </div>
-        <div className='features__shipping'>
-            <img src={money.src} className='features__img'></img>
-            <div className='features__text'>
-             <h1>Money Back</h1>
-             <h3>30 Days Guarranty</h3>
-            </div>
+        <div className="features__shipping">
+          <img src={money.src} className="features__img"></img>
+          <div className="features__text">
+            <h1>Money Back</h1>
+            <h3>30 Days Guarranty</h3>
+          </div>
         </div>
-        <div className='features__shipping'>
-            <img src={card.src} className='features__img'></img>
-            <div className='features__text'>
-             <h1>Secure Payment</h1>
-             <h3>All Cards Accepted</h3>
-            </div>
+        <div className="features__shipping">
+          <img src={card.src} className="features__img"></img>
+          <div className="features__text">
+            <h1>Secure Payment</h1>
+            <h3>All Cards Accepted</h3>
+          </div>
         </div>
       </div>
-      <div className='categories'>
-        <div className='categories__speakers'>
-            <div className='categories__warp'>
-                <h3>Blast.Past.Fast</h3>
-                <h1>Speakers With Signature sound</h1>
-                <button>Shop By Category</button>
-            </div>            
-          
-            <img  src={speaker.src}></img>
-        </div>
-        
-        <div className='categories__watches'>
-            <div className='categories__warp'>
-                <h3>Smart Watches</h3>
-                <h1>Best Smart Watched for Men & Women</h1>
-                <button>Shop By Category</button>
-            </div>
-            <img  src={watch.src}></img>        
+      <div className="categories">
+        <div className="categories__speakers">
+          <div className="categories__warp">
+            <h3>Blast.Past.Fast</h3>
+            <h1>Speakers With Signature sound</h1>
+            <button>Shop By Category</button>
+          </div>
+
+          <img src={speaker.src}></img>
         </div>
 
-        <div className='categories__headphones'>
-            <div className='categories__warp'>
-                <h3>Wireless HeadPhones</h3>
-                <h1>Own Every Match with Gaming HeadPhones</h1>
-                <button>Shop By Category</button>
-            </div>
-            <img  src={headphone.src}></img>
+        <div className="categories__watches">
+          <div className="categories__warp">
+            <h3>Smart Watches</h3>
+            <h1>Best Smart Watched for Men & Women</h1>
+            <button>Shop By Category</button>
+          </div>
+          <img src={watch.src}></img>
         </div>
 
-        <div className='categories__tws'>
-            <div className='categories__warp'>
-                <h3>EarBuds.</h3>
-                <h1>Top Rated & Best Earbuds</h1>
-                <button>Shop By Category</button>
+        <div className="categories__headphones">
+          <div className="categories__warp">
+            <h3>Wireless HeadPhones</h3>
+            <h1>Own Every Match with Gaming HeadPhones</h1>
+            <button>Shop By Category</button>
+          </div>
+          <img src={headphone.src}></img>
+        </div>
+
+        <div className="categories__tws">
+          <div className="categories__warp">
+            <h3>EarBuds.</h3>
+            <h1>Top Rated & Best Earbuds</h1>
+            <button>Shop By Category</button>
+          </div>
+          <img src={tws.src}></img>
+        </div>
+      </div>
+      <div className="product">
+        <h1 className="product__header">Top Rated Watches</h1>
+        <q className="product__tagline">
+          Empower Your Every Moment with Intuitive Connectivity: Unleash the
+          Future on Your Wrist.
+        </q>
+        <div className="product__content">
+          {watchesData.map((watch, index) => (
+            <div className="product__content__single">
+              <img src={watchList[index].src || watch.imageSrc}></img>
+              <h3>{watch.title}</h3>
+              <button>{watch.price}</button>
             </div>
-            <img  src={tws.src}></img>          
+          ))}
         </div>
       </div>
-      <div className='product'>
-        <h1 className='product__header'>Top Rated Watches</h1>
-        <q className='product__tagline'>Empower Your Every Moment with Intuitive Connectivity: Unleash the Future on Your Wrist.</q>
-        <div className='product__content'>
-               {watchesData.map((watch,index) => (
-       <div className='product__content__single'>
-       <img src={watchList[index].src||watch.imageSrc}></img>
-       <h3>{watch.title}</h3>
-       <button>{watch.price}</button>
-     </div>
-      ))}
-        </div>
-      </div>
-      <div className='banner'>
+      <div className="banner">
         <img src={redheadphone.src} className="banner__image"></img>
-        <div className='banner__left'>
+        <div className="banner__left">
           <h1>FIRST SALE</h1>
         </div>
-        <div className='banner__right'>
-            <h3 className='banner__right__header'>Get a welcome 50% off your first order</h3>
-            <button>Shop By category</button>
-
+        <div className="banner__right">
+          <h3 className="banner__right__header">
+            Get a welcome 50% off your first order
+          </h3>
+          <button>Shop By category</button>
         </div>
       </div>
-      <div className='product innerboxshadow'>
-        <h1 className='product__header'>Top Rated Earbuds</h1>
-        <q className='product__tagline'>Feel the beauty of sound..</q>
-        <div className='product__content'>
-               {watchesData.map((watch,index) => (
-       <div className='product__content__single'>
-       <img src={earbudsList[index].src||watch.imageSrc}></img>
-       <h3>{watch.title}</h3>
-       <button>{watch.price}</button>
-     </div>
-      ))}
+      <div className="product innerboxshadow">
+        <h1 className="product__header">Top Rated Earbuds</h1>
+        <q className="product__tagline">Feel the beauty of sound..</q>
+        <div className="product__content">
+          {watchesData.map((watch, index) => (
+            <div className="product__content__single">
+              <img src={earbudsList[index].src || watch.imageSrc}></img>
+              <h3>{watch.title}</h3>
+              <button>{watch.price}</button>
+            </div>
+          ))}
         </div>
       </div>
-      <div className='reviews'>
-        <div className='reviews__cards'>
-          <img src={headphone2.src} className='reviews__cards__hero'></img>
-          <q>The SwiftBeats X100 headphones redefine comfort and performance. Feather-light design and impeccable sound quality make these a must-have. Pure audio bliss in a compact package!</q>
-          <img src={avatar1.src}className='reviews__cards__user'></img>
+      <div className="reviews">
+        <div className="reviews__cards">
+          <img src={headphone2.src} className="reviews__cards__hero"></img>
+          <q>
+            The SwiftBeats X100 headphones redefine comfort and performance.
+            Feather-light design and impeccable sound quality make these a
+            must-have. Pure audio bliss in a compact package!
+          </q>
+          <img src={avatar1.src} className="reviews__cards__user"></img>
           <h3>John Doe</h3>
           <p>⭐️⭐️⭐️⭐️⭐️ </p>
         </div>
-        <div className='reviews__cards'>
-          <img src={tws.src} className='reviews__cards__hero'></img>
-          <q>The battery life is impressive too. I charged them once, and they lasted through a week of daily use. The Bluetooth connectivity is seamless, allowing you to dance, jog, or cook without being tethered to your device.</q>
-          <img src={avatar3.src}className='reviews__cards__user'></img>
+        <div className="reviews__cards">
+          <img src={tws.src} className="reviews__cards__hero"></img>
+          <q>
+            The battery life is impressive too. I charged them once, and they
+            lasted through a week of daily use. The Bluetooth connectivity is
+            seamless, allowing you to dance, jog, or cook without being tethered
+            to your device.
+          </q>
+          <img src={avatar3.src} className="reviews__cards__user"></img>
           <h3>John Doe</h3>
           <p>⭐️⭐️⭐️⭐️⭐️ </p>
         </div>
-        <div className='reviews__cards'>
-          <img src={watch5.src} className='reviews__cards__hero'></img>
-          <q>The SwiftBeats X100 headphones redefine comfort and performance. Feather-light design and impeccable sound quality make these a must-have. Pure audio bliss in a compact package!</q>
-          <img src={avatar1.src}className='reviews__cards__user'></img>
+        <div className="reviews__cards">
+          <img src={watch5.src} className="reviews__cards__hero"></img>
+          <q>
+            The SwiftBeats X100 headphones redefine comfort and performance.
+            Feather-light design and impeccable sound quality make these a
+            must-have. Pure audio bliss in a compact package!
+          </q>
+          <img src={avatar1.src} className="reviews__cards__user"></img>
           <h3>John Doe</h3>
           <p>⭐️⭐️⭐️⭐️⭐️ </p>
         </div>
