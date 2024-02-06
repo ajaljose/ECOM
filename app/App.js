d8dd9b5056619e8306acee767dff9621e993e083
Ajal Jose <ajaljosem@gmail.com>
2024-02-06 14:16:11 +0530

    navbar

--- app/App.js
+++ app/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Home from "./Home/Home";
-
+import {Routes,Route} from 'react-router-dom';
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <>
+      <Navbar/>
       <Home />
     </>
   );
--- app/Home/Home.js
+++ app/Home/Home.js
@@ -1,5 +1,4 @@
 import React, { useState  } from 'react';
-import github from "../images/logo-white.png";
 import hero1 from "../images/hero1.png";
 import hero2 from "../images/iphone.png";
 import left from "../images/left.png";
@@ -62,54 +61,7 @@ function Home() {
   };
   return (
     <div className="home">
-      <div className="navbar">
-        <img
-          src={github.src}
-          alt="GitHub Logo"
-          className="navbar__logo"
-          title="URBAN KING"
-        />
-
-        <div className="navbar__menu">
-          <div className="container">
-            <input
-              type="text"
-              name="text"
-              className="input"
-              required
-              placeholder="Type to search..."
-            />
-            <div className="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="ionicon"
-                viewBox="0 0 512 512"
-              >
-                <title>Search</title>
-                <path
-                  d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                  strokeWidth="32"
-                ></path>
-                <path
-                  fill="#000000"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="32"
-                  d="M338.29 338.29L448 448"
-                ></path>
-              </svg>
-            </div>
-          </div>
-          <h3>Home</h3>
-          <h3>Shop</h3>
-          <h3>Blog</h3>
-          <h3>Contact</h3>
-        </div>
-      </div>
+     
       <div className="hero" style={gradientStyle}>
         <div className="hero__images">
           <img
