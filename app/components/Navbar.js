37636f02fff8d6fe64491bcc8e081ac264f44428
Ajal Jose <ajaljosem@gmail.com>
2024-02-07 20:39:59 +0530

    react router to search page

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from "react";
 import github from "../images/logo-white.png";
-import { Link } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 function Navbar() {
+  const [inputValue, setInputValue] = useState("");
+  const navigate = useNavigate();
+  const handleClick = () => {
+    if (inputValue.trim() === "") {
+      navigate('/');
+    } else {
+      navigate('search');
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
   return (
-    <div>     
-     <div className="navbar">
+    <div>
+      <div className="navbar">
         <img
           src={github.src}
           alt="GitHub Logo"
@@ -13,47 +26,42 @@ function Navbar() {
         />
 
         <div className="navbar__menu">
-        <Link to='/search'><div className="container">
-            <input
-              type="text"
-              name="text"
-              className="input"
-              required
-              placeholder="Type to search..."
-            />
-            <div className="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="ionicon"
-                viewBox="0 0 512 512"
-              >
-                <title>Search</title>
-                <path
-                  d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeMiterlimit="10"
-                  strokeWidth="32"
-                ></path>
-                <path
-                  fill="#000000"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeMiterlimit="10"
-                  strokeWidth="32"
-                  d="M338.29 338.29L448 448"
-                ></path>
-              </svg>
-            </div>
-          </div></Link>
+          <div className="navbar__menu__search">
+            <input placeholder="Search..." type="text" onChange={handleInputChange}/>
+              <button type="submit" className="icon" onClick={handleClick}>
+                
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="ionicon"
+                  viewBox="0 0 512 512"
+                >
+                  <title>Search</title>
+                  <path
+                    d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeMiterlimit="10"
+                    strokeWidth="32"
+                  ></path>
+                  <path
+                    fill="#000000"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeMiterlimit="10"
+                    strokeWidth="32"
+                    d="M338.29 338.29L448 448"
+                  ></path>
+                </svg>
+              </button>
+          </div>
           <h3>Home</h3>
           <h3>Shop</h3>
           <h3>Blog</h3>
           <h3>Contact</h3>
         </div>
       </div>
-          </div>
-  )
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- app/components/Search.js
+++ app/components/Search.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 function Search() {
+  useEffect(() => {
+    // This code will be executed when the component is mounted (onload)
+    // alert('Component is loaded!');
+  }, []); // The empty dependency array ensures the effect runs only once on mount
+
   return (
     <div>Search</div>
-  )
+  );
 }
 
-export default Search
+export default Search;
