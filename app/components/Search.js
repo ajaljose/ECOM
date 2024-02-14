7faa341c82fcc4bb33ead746b02b526016d39460
Ajal Jose <ajaljosem@gmail.com>
2024-02-14 22:43:45 +0530

    formatted

--- app/components/Search.js
+++ app/components/Search.js
@@ -16,41 +16,40 @@ function Search() {
 
   return (
     <>
-     <div className="search">
-      <div className="search__filter">
-        {/* <div className="search__filter__content"> */}
-        <h3>MAX PRICE</h3>
-        <input id='price'type='range' min="0" max="100" ></input>
-        {/* </div> */}
-        <hr></hr>
-        <div className="search__filter__content">
-        <h3>Customer Ratings</h3>
-        <input type='checkbox' id='fourStar'></input>
-        <label htmlFor='fourStar'>4 Stars</label>
-        <input type='checkbox' id='threeStar'></input>
-        <label htmlFor='threeStar'>3 Stars</label>
-        <input type='checkbox' id='twoStar'></input>
-        <label htmlFor='twoStar'>2 Stars</label>
-        <input type='checkbox' id='oneStar'></input>
-        <label htmlFor='oneStar'>1 Stars</label>
-        </div>
-        <hr></hr>
-        
-        <button>Apply</button>
-      </div>
-      <div className="search__content">
-        {products.map((watch, index) => (
-          <div className="search__content__single">
-            <img src={watch.image}></img>
-            <h3>{watch.title}</h3>
-            <h1>{watch.price}</h1>
+      <div className="search">
+        <div className="search__filter">
+          {/* <div className="search__filter__content"> */}
+          <h3>MAX PRICE</h3>
+          <input id="price" type="range" min="0" max="100"></input>
+          {/* </div> */}
+          <hr></hr>
+          <div className="search__filter__content">
+            <h3>Customer Ratings</h3>
+            <input type="checkbox" id="fourStar"></input>
+            <label htmlFor="fourStar">4 Stars</label>
+            <input type="checkbox" id="threeStar"></input>
+            <label htmlFor="threeStar">3 Stars</label>
+            <input type="checkbox" id="twoStar"></input>
+            <label htmlFor="twoStar">2 Stars</label>
+            <input type="checkbox" id="oneStar"></input>
+            <label htmlFor="oneStar">1 Stars</label>
           </div>
-        ))}
-      </div>      
-    </div>
-    <Footer/>
+          <hr></hr>
+
+          <button>Apply</button>
+        </div>
+        <div className="search__content">
+          {products.map((watch, index) => (
+            <div className="search__content__single">
+              <img src={watch.image}></img>
+              <h3>{watch.title}</h3>
+              <h1>{watch.price}</h1>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer />
     </>
-   
   );
 }
 
