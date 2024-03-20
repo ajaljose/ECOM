242647f9287b0e83f78ad0daf6a37b88bcc1a892
Ajal Jose <ajaljosem@gmail.com>
2024-03-20 19:54:27 +0530

    cart css

--- app/components/CartDetails.js
+++ app/components/CartDetails.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function CartDetails() {
-  return <>Cart</>;
+  return <>
+  <div className="cart">
+    <h1 className="cart__header">Cart</h1>
+    <hr></hr>
+    <div className="cart__list"></div>
+  </div>
+  
+  </>;
 }
 
 export default CartDetails;
