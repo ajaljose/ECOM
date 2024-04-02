633de002bfdd8fe33fede1255fcc14b8d9fc1975
Ajal Jose <ajaljosem@gmail.com>
2024-04-02 18:58:23 +0530

    e com 99

--- app/components/Product.js
+++ app/components/Product.js
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from "react";
 import Footer from "./Footer";
 import axios from "axios";
 import Loader from "./Loader";
+import { useAppContext } from "@/context";
+import { useRouter } from "next/navigation";
 function Product(props) {
   const [product, setProduct] = useState({});
+  const [Qnty, setQnty] = useState(1);
+  const { cartList,setCartList } = useAppContext();
+  const router=useRouter();
   useEffect(() => {
     try {
       let url = "https://fakestoreapi.com/products/" + props.productId;
@@ -15,6 +20,27 @@ function Product(props) {
       console.error(error);
     }
   },[]);
+  const handleAddtoCart=(item)=>{
+  // console.log(item);
+  let mFlag=false;
+  let temp_cartList=cartList;
+    for(let i=0;i<temp_cartList.length;i++){
+      if(temp_cartList[i].id==item.id){
+        temp_cartList[i].quantity=item.quantity;
+        mFlag=true;
+        break;
+      }
+    }
+    mFlag?setCartList(temp_cartList):(item.quantity=1,setCartList([...cartList,item]));
+   }
+   const decreaseQnty=()=>{
+      if(Qnty-1==0) alert('Minimum 1 quantity is required');
+      else{setQnty(Qnty-1)}
+   }
+   const buyNow=async (item)=>{
+    await handleAddtoCart(item);
+    router.replace('/cart');
+   }
   return (
     <>{Object.keys(product).length==0?<Loader/>:''}
       <div className="product">
@@ -29,20 +55,32 @@ function Product(props) {
             <p>{product.description}</p>
             <div className="product__hero__right__addtocart">
               <div className="btn__increment" id="itemCount">
-                <button className="btn__increment__min">-</button>
+                <button className="btn__increment__min" onClick={(e)=>decreaseQnty()}>-</button>
                 <input
                   type="number"
                   className="btn__increment__mid"
-                  placeholder="0"
+                  placeholder={Qnty}
                   disabled
                 ></input>
-                <button className="btn__increment__add">+</button>
+                <button className="btn__increment__add" onClick={(e)=>setQnty(Qnty+1)}>+</button>
               </div>
-              <button className="primary__btn1" id="btnAddCart">
+              <button className="primary__btn1" id="btnAddCart" onClick={(e)=>handleAddtoCart({
+                      id: product.id,
+                      title: product.title,
+                      price: product.price,
+                      image: product.image,
+                      quantity: Qnty,
+                    })}>
                 ADD to cart
               </button>
             </div>
-            <button className="primary__btn1" id="btnBuyNow">
+            <button className="primary__btn1" id="btnBuyNow" onClick={(e)=>buyNow({
+                      id: product.id,
+                      title: product.title,
+                      price: product.price,
+                      image: product.image,
+                      quantity: Qnty,
+                    })}>
               Buy Now
             </button>
           </div>
